e8e9b0b55913a399eae66053e72856f5f98a098a
marzeckif <filipo085@icloud.com>
2022-05-29 12:08:51 +0200

    cocktailSort dodany oraz jego wyswietlenie

--- main.cpp
+++ main.cpp
@@ -1,62 +1,14 @@
 #include <iostream>
-
-void cocktailSort(int tableToSort[], int tableSize)
-{
-
-    bool swapped = true;
-    int min = 0;
-    int max = tableSize - 1;
-    int liczba_porownan = 0;
-    int liczba_przestawien = 0;
-
-    while (swapped)
-    {
-        swapped = false;
-        for (int i = min; i < max; i++)
-        {
-            liczba_porownan++;
-            if (tableToSort[i] > tableToSort[i + 1])
-            {
-                std::swap(tableToSort[i], tableToSort[i + 1]);
-                swapped = true;
-                liczba_przestawien++;
-            }
-        }
-
-        if (!swapped)
-            break;
-
-        swapped = false;
-        max--;
-
-        for (int i = max; i > min; i--)
-        {
-            liczba_porownan++;
-            if(tableToSort[i] < tableToSort[i-1]){
-                std::swap(tableToSort[i], tableToSort[i-1]);
-                swapped = true;
-                liczba_przestawien++;
-            }
-        }
-        min++;
-    }
-
-
-    std:: cout << "Liczba przestawien: " << liczba_przestawien;
-    std:: cout << "Liczba porownan: " << liczba_porownan << "\n";
-}
-
-void printTable(int tableToSort[], int tableSize) {
-    for (int i = 0; i < tableSize; i++)
-    {
-        std::cout << tableToSort[i];
-    }
-}
+#include <climits>
+#include "CSortTablicy.h"
+#include "CTablica.h"
 
 int main()
 {
-    int tabToSort[]{4, 2, 3, 6, 1};
-    cocktailSort(tabToSort,5);
-    printTable(tabToSort,5);
+
+    CTablica sortINT;
+    CSortTablicy CsortINT;
+    CsortINT.ilustrateMethods();
+
     return 0;
 }
